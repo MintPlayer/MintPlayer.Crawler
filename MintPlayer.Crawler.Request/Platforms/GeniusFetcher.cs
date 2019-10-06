1e20a61f400e276ff72d01a53e7ff8f738041aef
MusicDemons <pieterjandeclippel@hotmail.com>
2019-10-06 16:33:40 +0200

    Working version

--- MintPlayer.Crawler.Request/Platforms/GeniusFetcher.cs
+++ MintPlayer.Crawler.Request/Platforms/GeniusFetcher.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -33,12 +34,7 @@ namespace MintPlayer.Crawler.Request.Platforms
                     var recording = JsonConvert.DeserializeObject<MusicRecording>(ld_json);
 
                     // Read lyrics
-                    var lyricsRegex = new Regex(@"(?<=\<div class=\""lyrics\"">).*?(?=\<\/div\>)", RegexOptions.Singleline | RegexOptions.Multiline);
-                    var lyricsMatch = lyricsRegex.Match(Html);
-
-                    if (!lyricsMatch.Success) throw new Exception("No lyrics tag found");
-
-                    recording.Lyrics = TrimLyrics(lyricsMatch.Value);
+                    recording.Lyrics = ExtractLyrics(Html);
 
                     return recording;
                 case "MusicGroup":
@@ -49,9 +45,25 @@ namespace MintPlayer.Crawler.Request.Platforms
             }
         }
 
-        private string TrimLyrics(string lyrics)
+        private string ExtractLyrics(string html)
         {
+            var lyricsRegex = new Regex(@"(?<=\<div class=\""lyrics\""\>).*?(?=\<\/div\>)", RegexOptions.Singleline | RegexOptions.Multiline);
+            var lyricsMatch = lyricsRegex.Match(html);
+            if (!lyricsMatch.Success) throw new Exception("No lyrics tag found");
 
+            var pRegex = new Regex(@"(?<=\<p\>).*?(?=\<\/p\>)", RegexOptions.Singleline | RegexOptions.Multiline);
+            var pMatch = pRegex.Match(lyricsMatch.Value);
+            if (!pMatch.Success) throw new Exception("No P tag found");
+
+            var aRegex = new Regex(@"\<a.*?\>(?<text>.*?)\<\/a\>", RegexOptions.Singleline | RegexOptions.Multiline);
+            var lines = aRegex.Matches(pMatch.Value);
+            var arrLines = new Match[lines.Count];
+            lines.CopyTo(arrLines, 0);
+
+            var stripped = arrLines.Select(l => l.Groups["text"].Value);
+            return string.Join(Environment.NewLine, stripped);
+
+            //return rule?.tex;
         }
     }
 }
