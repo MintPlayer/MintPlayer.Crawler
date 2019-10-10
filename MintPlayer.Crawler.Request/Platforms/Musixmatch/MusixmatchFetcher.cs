9235dc728840e2c2f2f369a92346dd99d058e266
MusicDemons <pieterjandeclippel@hotmail.com>
2019-10-11 00:26:37 +0200

    Musixmatch fetch lyrics

--- MintPlayer.Crawler.Request/Platforms/Musixmatch/MusixmatchFetcher.cs
+++ MintPlayer.Crawler.Request/Platforms/Musixmatch/MusixmatchFetcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -21,7 +22,23 @@ namespace MintPlayer.Crawler.Request.Platforms.Musixmatch
         public override async Task<Subject> Fetch(HttpClient httpClient, bool trimTrash)
         {
             var html = await SendRequest(httpClient);
-            return null;
+            var result = new Data.Song
+            {
+                Lyrics = ExtractLyrics(html, true)
+            };
+            return result;
+        }
+
+        private string ExtractLyrics(string html, bool trimTrash)
+        {
+            var spanRegex = new Regex(@"(?<=\<span class=\""lyrics__content__ok\""\>).*?(?=\<\/span\>)", RegexOptions.Singleline | RegexOptions.Multiline);
+            var spanMatches = spanRegex.Matches(html);
+            if (spanMatches.Count == 0) throw new Exception("span tag not found");
+
+            var matches = new Match[spanMatches.Count];
+            spanMatches.CopyTo(matches, 0);
+
+            return string.Join("\r\n\r\n", matches.Select(m => m.Value));
         }
     }
 }
--- MintPlayer.Crawler.Test/Program.cs
+++ MintPlayer.Crawler.Test/Program.cs
@@ -11,13 +11,14 @@ namespace MintPlayer.Crawler.Test
 
             var crawler = new MintPlayerCrawler();
             //var res = crawler.GetByUrl("https://genius.com/Daft-punk-get-lucky-lyrics", true).Result;
-            var res0 = crawler.GetByUrl("https://genius.com/artists/Daft-punk", true).Result;
+            //var res0 = crawler.GetByUrl("https://genius.com/artists/Daft-punk", true).Result;
             //var res1 = crawler.GetByUrl("https://genius.com/albums/Daft-punk/Random-access-memories", true).Result;
 
-            //var res2 = crawler.GetByUrl("https://www.musixmatch.com/lyrics/Daft-Punk-feat-Nile-Rodgers-Pharrell-Williams/Get-lucky", true).Result;
+            var res2 = crawler.GetByUrl("https://www.musixmatch.com/lyrics/Daft-Punk-feat-Nile-Rodgers-Pharrell-Williams/Get-lucky", true).Result;
 
             //var res3 = crawler.GetByUrl("https://songteksten.net/lyric/97/96025/daft-punk/get-lucky.html", true).Result;
             //var res4 = crawler.GetByUrl("https://songteksten.net/artist/lyrics/97/daft-punk.html", true).Result;
+            //var res5 =  crawler.GetByUrl("https://genius.com/Avicii-sunset-jesus-lyrics", true).Result;
         }
     }
 }
